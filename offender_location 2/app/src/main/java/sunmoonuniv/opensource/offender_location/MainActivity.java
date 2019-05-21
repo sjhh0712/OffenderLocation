4d77e3f163754d50350b6be9e781ebe3cc666336
sjhh0712 <sjhh0712@naver.com>
2019-05-21 20:36:35 +0900

    offender_location 프레그먼트 적용(로그인x)

---
+++ offender_location 2/app/src/main/java/sunmoonuniv/opensource/offender_location/MainActivity.java
@@ -0,0 +1,85 @@
+package sunmoonuniv.opensource.offender_location;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), menu2Activity.class);
+                startActivityForResult(intent, 101);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101){
+            String name = data.getStringExtra("name");
+            Toast.makeText(getApplicationContext(), "메뉴화면으로부터 응답 :" + name, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch (item.getItemId()){
+
+            case R.id.navigation_dashboard:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_notifications:
+                fragment = new DashboardFragment();
+                break;
+
+            case R.id.navigation_location:
+                fragment = new NotificationFragment();
+                break;
+
+        }
+
+        return loadFragment(fragment);
+    }
+}
